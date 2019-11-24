dc156aa92363d9170ac6d9a747d17a96361450bf
EdenGranot <37970771+EdenGranot@users.noreply.github.com>
2019-11-24 22:17:18 +0200

    added distance from target and angle

---
+++ constants.py
@@ -0,0 +1,6 @@
+# pixels (mey be 333.82)
+FOCAL_LENGTH = 697.0395744431028
+# m^2
+STICKER_AREA = 5 * 14 / 100 ** 2
+# radiance (may be 1.19555054)
+FOV = 0.4228539261329407
--- openCVTest.py
+++ openCVTest.py
@@ -1,5 +1,22 @@
 import numpy as np
+import math
 import cv2
+import constants
+
+
+def d(pt1, pt2):
+    return math.sqrt((pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2)
+
+
+def sticers(img):
+    frameHSV = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    frameHSV = cv2.inRange(frameHSV, np.array([0, 180, 85]), np.array([100, 255, 255]))
+    frameHSV = cv2.filter2D(frameHSV, -1, np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]))
+    frameHSV = cv2.medianBlur(frameHSV, 27)
+
+    contours, hierarchy = cv2.findContours(frameHSV, 1, 2)
+
+    frameHSV = cv2.cvtColor(frameHSV, cv2.COLOR_GRAY2RGB)
 
 
 def runtime():
@@ -24,36 +41,94 @@ def prossace():
     i = 0
     while 1:
         frame = cv2.imread(f"img {i}.png")
-        frameHSV = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-        frameHSV = cv2.inRange(frameHSV, np.array([0, 180, 85]), np.array([100, 255, 255]))
-        frameHSV = cv2.filter2D(frameHSV, -1, np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]))
-        frameHSV = cv2.medianBlur(frameHSV, 27)
-        cv2.imshow("show", frameHSV)
+        frame_hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+        frame_hsv = cv2.inRange(frame_hsv, np.array([0, 180, 85]), np.array([100, 255, 255]))
+        frame_hsv = cv2.filter2D(frame_hsv, -1, np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]]))
+        frame_hsv = cv2.medianBlur(frame_hsv, 27)
+
+        height, width = frame_hsv.shape
+
+        contours, hierarchy = cv2.findContours(frame_hsv, 1, 2)
+
+        frame_hsv = cv2.cvtColor(frame_hsv, cv2.COLOR_GRAY2RGB)
+        if contours:
+            if len(contours) == 1:
+                rect = cv2.minAreaRect(contours[0])
+
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+
+                D = constants.FOCAL_LENGTH * math.sqrt(constants.STICKER_AREA / (d(box[0], box[1]) * d(box[0], box[3])))
+
+                print(f"D = {D}")
+                cv2.drawContours(frame_hsv, [box], -1, (0, 0, 255), 2)
+
+            elif len(contours) == 2:
+                rectL = cv2.minAreaRect(contours[0])
+                rectR = cv2.minAreaRect(contours[1])
+
+                boxL = cv2.boxPoints(rectL)
+                boxL = np.int0(boxL)
+                DL = constants.FOCAL_LENGTH * math.sqrt(
+                    constants.STICKER_AREA / (d(boxL[0], boxL[1]) * d(boxL[0], boxL[3])))
+                pixel_middle_L = (boxL[0] + boxL[3]) / 2
+
+                boxR = cv2.boxPoints(rectR)
+                boxR = np.int0(boxR)
+                DR = constants.FOCAL_LENGTH * math.sqrt(
+                    constants.STICKER_AREA / (d(boxR[0], boxR[1]) * d(boxR[0], boxR[3])))
+                pixel_middle_R = (boxR[0] + boxR[3]) / 2
+
+                if pixel_middle_L[0] > pixel_middle_R[1]:
+                    tempBox = boxR
+                    tempD = DR
+                    tempPixel_middle = pixel_middle_R
+
+                    boxR = boxL
+                    DR = DL
+                    pixel_middle_R = pixel_middle_L
+
+                    boxL = tempBox
+                    DL = tempD
+                    pixel_middle_L = tempPixel_middle
+
+                if pixel_middle_L[0] > width / 2:
+                    D = DL
+                    pixel_middle = pixel_middle_L
+                else:
+                    D = DR
+                    pixel_middle = pixel_middle_R
+
+                Dx = D * math.sin(constants.FOV * 2 * (pixel_middle[0]) / width)
+                Dy = D * math.sin(constants.FOV * 2 * (pixel_middle[1]) / height)
+                print(pixel_middle_L + pixel_middle_R)
+                print(f"D = {D}, Dx = {Dx}, Dy = {Dy}")
+                Dz = math.sqrt(D ** 2 - Dx ** 2 - Dy ** 2)
+                print(f"Dx = {Dx}, Dy = {Dy}, Dz = {Dz}")
+
+                angle = math.atan(Dx / Dz)
+
+                print(f"D = {D}, angle = {math.degrees(angle)}")
+                cv2.drawContours(frame_hsv, [boxL], -1, (0, 0, 255), 2)
+                cv2.drawContours(frame_hsv, [boxR], -1, (0, 0, 255), 2)
+
+        cv2.imshow("show", frame_hsv)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         if cv2.waitKey(1) & 0xFF == ord('n'):
             i += 1
+            print(i)
 
-        contours, hierarchy = cv2.findContours(frameHSV, 1, 2)
-        if contours:
-            if len(contours) == 1:
-                rect = cv2.minAreaRect(contours[0])
-                M = (cv2.boxPoints(rect)[0] + cv2.boxPoints(rect)[1] + cv2.boxPoints(rect)[2] + cv2.boxPoints(rect)[3]) / 4
-                print("1 rects", M)
-            elif len(contours) == 2:
-                rectL = cv2.minAreaRect(contours[0])
-                rectR = cv2.minAreaRect(contours[1])
-                leftM = (cv2.boxPoints(rectL)[0] + cv2.boxPoints(rectL)[1] + cv2.boxPoints(rectL)[2] + cv2.boxPoints(rectL)[3]) / 4
-                rightM = (cv2.boxPoints(rectR)[0] + cv2.boxPoints(rectR)[1] + cv2.boxPoints(rectR)[2] + cv2.boxPoints(rectR)[3]) / 4
-                print("2 rects", leftM, rightM)
-        i += 1
+        #i += 1
         i = i % 264
     cv2.destroyAllWindows()
 
 
 def main() -> object:
-    print("starting: proccase")
+    print(f"starting: proccase")
+    cv2.waitKey(1)
     prossace()
+
 
 if __name__ == '__main__':
     main()
