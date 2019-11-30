ebf9a96457e7a2bdc6b0accc6dcfcf8843691b7b
EdenGranot <37970771+EdenGranot@users.noreply.github.com>
2019-11-30 20:20:39 +0200

    V5 comments

--- openCVTest.py
+++ openCVTest.py
@@ -5,27 +5,36 @@ import constants
 import json
 
 
+# distance between two points
 def d(pt1, pt2):
     return math.sqrt((pt1[0] - pt2[0]) ** 2 + (pt1[1] - pt2[1]) ** 2)
 
 
 def distance_angle_frame(img, min_color, max_color, blur_val):
+    # convert image to hsv
     frame_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+    # threshold
     frame_hsv = cv2.inRange(frame_hsv, min_color, max_color)
+    # blur
     frame_hsv = cv2.medianBlur(frame_hsv, blur_val)
 
     height, width = frame_hsv.shape
 
-    contours, hierarchy = cv2.findContours(frame_hsv, 1, 2)
+    # find objects
+    contours, _ = cv2.findContours(frame_hsv, 1, 2)
 
+    # find the object in rectangles and apply formulas
     frame_hsv = cv2.cvtColor(frame_hsv, cv2.COLOR_GRAY2RGB)
     if contours:
         if len(contours) == 1:
+            # gets the smallest rectangle that block the contour
             rect = cv2.minAreaRect(contours[0])
 
+            # convert to box object
             box = cv2.boxPoints(rect)
             box = np.int0(box)
 
+            # avoid dividing by 0
             if d(box[0], box[1]) * d(box[0], box[3]) == 0:
                 return None, None, frame_hsv
             D = constants.FOCAL_LENGTH * math.sqrt(constants.STICKER_AREA / (d(box[0], box[1]) * d(box[0], box[3])))
@@ -34,6 +43,7 @@ def distance_angle_frame(img, min_color, max_color, blur_val):
             Dx = D * math.sin(constants.FOV * 2 * (pixel_middle[0]) / width)
             Dy = D * math.sin(constants.FOV * 2 * (pixel_middle[1]) / height)
             Dz = D ** 2 - Dx ** 2 - Dy ** 2
+            # avoid square root negative number
             if Dz < 0:
                 return None, None, frame_hsv
             Dz = math.sqrt(Dz)
@@ -49,6 +59,7 @@ def distance_angle_frame(img, min_color, max_color, blur_val):
 
             boxL = cv2.boxPoints(rectL)
             boxL = np.int0(boxL)
+            # avoid dividing by 0
             if d(boxL[0], boxL[1]) * d(boxL[0], boxL[3]) == 0:
                 return None, None, frame_hsv
             DL = constants.FOCAL_LENGTH * math.sqrt(
@@ -57,24 +68,12 @@ def distance_angle_frame(img, min_color, max_color, blur_val):
 
             boxR = cv2.boxPoints(rectR)
             boxR = np.int0(boxR)
+            # avoid dividing by 0
             if d(boxR[0], boxR[1]) * d(boxR[0], boxR[3]) == 0:
                 return None, None, frame_hsv
             DR = constants.FOCAL_LENGTH * math.sqrt(
                 constants.STICKER_AREA / (d(boxR[0], boxR[1]) * d(boxR[0], boxR[3])))
             pixel_middle_R = (boxR[0] + boxR[3]) / 2
-
-            if pixel_middle_L[0] > pixel_middle_R[1]:
-                tempBox = boxR
-                tempD = DR
-                tempPixel_middle = pixel_middle_R
-
-                boxR = boxL
-                DR = DL
-                pixel_middle_R = pixel_middle_L
-
-                boxL = tempBox
-                DL = tempD
-                pixel_middle_L = tempPixel_middle
 
             D = (DL + DR) / 2
             pixel_middle = (pixel_middle_L + pixel_middle_R) / 2
@@ -82,6 +81,7 @@ def distance_angle_frame(img, min_color, max_color, blur_val):
             Dx = D * math.sin(constants.FOV * 2 * (pixel_middle[0]) / width)
             Dy = D * math.sin(constants.FOV * 2 * (pixel_middle[1]) / height)
             Dz = D ** 2 - Dx ** 2 - Dy ** 2
+            # avoid square root negative number
             if Dz < 0:
                 return None, None, frame_hsv
             Dz = math.sqrt(Dz)
@@ -95,21 +95,25 @@ def distance_angle_frame(img, min_color, max_color, blur_val):
 
 
 def main():
+    # load vision data
     data = json.load(open("custom.json", "r"))
     light = data["light"]
     blur = data["blur"]
     min_hsv = np.array(data["min"])
     max_hsv = np.array(data["max"])
 
+    # camera configuration
     cap = cv2.VideoCapture(0)
     cap.set(15, light)
     i = 0
     while True:
+        # reads the frame from the camera
         _, frame = cap.read()
-        #frame = cv2.imread(f"img {i}.png")
-        cv2.imshow("original", frame)
+        # frame = cv2.imread(f"img {i}.png")
 
+        # get the distance, angle and the edited frame
         D, angle, frame_edited = distance_angle_frame(frame, min_hsv, max_hsv, blur)
+        # show the original and edited images
         cv2.imshow("original", frame)
         cv2.imshow("processed", frame_edited)
         print(f"D {D}, angle {angle}")
